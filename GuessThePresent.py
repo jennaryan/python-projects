944e2826919fe4f658ae9885e5e0448d0651eb69
jennaryan <jennaryan07@gmail.com>
2014-12-01 16:51:18 -0500

    GTP
    Development of gtp

---
+++ GuessThePresent.py
@@ -0,0 +1,45 @@
+import time
+contents = [
+    'an old boot',
+    'coal',
+    'a teddy bear',
+    'a reindeer',
+
+    ]
+    
+
+print('''
+
+ _____                       _____ _           ______                         _   
+|  __ \                     |_   _| |          | ___ \                       | |  
+| |  \/_   _  ___  ___ ___    | | | |__   ___  | |_/ / __ ___  ___  ___ _ __ | |_ 
+| | __| | | |/ _ \/ __/ __|   | | | '_ \ / _ \ |  __/ '__/ _ \/ __|/ _ \ '_ \| __|
+| |_\ \ |_| |  __/\__ \__ \   | | | | | |  __/ | |  | | |  __/\__ \  __/ | | | |_ 
+ \____/\__,_|\___||___/___/   \_/ |_| |_|\___| \_|  |_|  \___||___/\___|_| |_|\__|
+                                                                                     ''')
+                                                                                  
+    
+print('Welcome to GTF! I will explain how to play in a second!')
+time.sleep(1)
+print('''(NOTE: Best use of this game is in full screen!) In this game you must
+help Santa choose the right present to bring to the kid!''')
+time.sleep(1)
+print(''' How to play: There will be three presents. You will see them, then they
+will move around. You will guess which one is correct. Now this is a guessing game,
+don't be mad if you get it wrong''')
+time.sleep(1)
+print('''Tired of waiting? Well it is time to play! Ready? Here we goooooo''')
+time.sleep(2)
+print('===================================================')
+time.sleep(0.5)
+print('''Santa needs help finding Jim`s present! Remember! This is just guessing!.''')
+time.sleep(3)
+present = input('''Finished! Now! What number do you think Jim's present is: 1, 2, or 3?''').lower().strip()
+if '1' in present:
+    
+
+
+
+
+
+

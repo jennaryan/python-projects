d1317b761b7fe54a17eaeeee2f664ab44e837c9d
jennaryan <jennaryan07@gmail.com>
2014-12-08 17:17:13 -0500

    BRPG

---
+++ B_RPG.py
@@ -0,0 +1,26 @@
+import time
+
+def archer():
+    print('''Alright! Your First Mission is To Take
+Over The British Weapon Site
+      ''')
+
+print('''
+
+____   ___  _____ _____ __    _____    _____ _____  ____  
+||=)  ||=||  ||    ||   ||    ||==     ||_// ||_// (( ___ 
+||_)) || ||  ||    ||   ||__| ||___    || \\ ||     \\_||
+''')
+print('''TO PLAY IN THE BEST WAY PUT ON FULL SCREEN''')
+time.sleep(1)
+print('''General Kerald: 'Welcome! You have finished training, so
+today, you will pick your class!''')
+time.sleep(1)
+classpick = input('''Now, what class do you want? Archer, Knight,
+or Ninja?''').lower().st
+if 'archer' in question:
+    def archer
+    
+
+      
+
